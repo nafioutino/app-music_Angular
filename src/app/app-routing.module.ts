4992f60305a8a0974c6677a230f29285647a6cee
Abdoul-Nafiou <nafiou531@gmail.com>
2024-08-12 18:44:21 +0100

    style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, RouterOutlet, Routes } from '@angular/router';
 import { AlbumsComponent } from './albums/albums.component';
 import { LoginComponent } from './login/login.component';
 import { AlbumDetailsComponent } from './album-details/album-details.component';
 import { SingleAlbumComponent } from './single-album/single-album.component';
-
 const routes: Routes = [
   {path:"albums", component:AlbumsComponent},
   {path:"", redirectTo:'/album', pathMatch:'full'},
