046d4a5fac22a1342a37d702d6580ccc8f49989c
Abdoul-Nafiou <nafiou531@gmail.com>
2024-08-13 09:45:17 +0100

    blabla

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -1,35 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { Album } from '../album';
-import { ALBUMS } from '../mock-albums';
 import { AlbumService } from '../services/album.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
-  styleUrl: './albums.component.css',
+  styleUrls: ['./albums.component.css'],
 })
 export class AlbumsComponent implements OnInit {
-  titlePage: string = 'Page principal Albums Music';
-
+  titlePage: string = 'Page principale Albums Music';
+  perPage = 2; // Nombre d'albums par page
+  searchQuery: string = ''; // Propriété pour stocker la requête de recherche
   albums!: Album[];
-  selectedAlbum!:Album;
-  hasClickDetails:boolean = false;
-  constructor(private albumService: AlbumService, private router:Router) { }
-  
+  filteredAlbums!: Album[];
+  selectedAlbum!: Album;
+  hasClickDetails: boolean = false;
+
+  constructor(private albumService: AlbumService, private router: Router) {}
+
   ngOnInit(): void {
-    this.albums = this.albumService.getAlbums()
+    this.updateAlbums(1);
+    
+    // S'abonner aux changements de page
+    this.albumService.sendCurrentNumberPage.subscribe(pageNumber => {
+      this.updateAlbums(pageNumber);
+    });
   }
 
-  onViewAlbum(id:string){
-    this.selectedAlbum = this.albumService.getalbumById(id)
-    this.router.navigateByUrl(`album/${this.selectedAlbum.id}`)
-    console.log(this.selectedAlbum.id);
-    
+  updateAlbums(pageNumber: number): void {
+    this.albums = this.albumService.paginateAlbums(pageNumber, this.perPage);
+    this.filterAlbums();
   }
-  
-  onSelect(id:string) {
-    this.selectedAlbum = this.albumService.getalbumById(id)
-    this.hasClickDetails = !this.hasClickDetails
+
+  filterAlbums(): void {
+    if (this.searchQuery.trim()) {
+      this.filteredAlbums = this.albums.filter(album => album.title.toLowerCase().includes(this.searchQuery.toLowerCase()));
+    } else {
+      this.filteredAlbums = this.albums;
+    }
+  }
+
+  onSearch(): void {
+    this.filterAlbums();
+  }
+
+  onViewAlbum(id: string) {
+    this.selectedAlbum = this.albumService.getAlbumById(id);
+    this.router.navigateByUrl(`album/${this.selectedAlbum.id}`);
+    console.log(this.selectedAlbum.id);
+  }
+
+  onSelect(id: string) {
+    this.selectedAlbum = this.albumService.getAlbumById(id);
+    this.hasClickDetails = !this.hasClickDetails;
   }
 }
